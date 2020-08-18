5eda4d3aa6d5ac7e6825aacbd390858b5b0d705b
Faris Widyantho <radenfaris@gmail.com>
2020-08-18 14:49:27 +0700

    handle back button

--- src/screens/WebviewScreen.js
+++ src/screens/WebviewScreen.js
@@ -3,19 +3,55 @@ import {
   View,
   Text,
   ActivityIndicator,
+  BackHandler,
 } from 'react-native'
 import { WebView } from 'react-native-webview';
-import Loading from '../components/Loading'
 
+import Loading from '../components/Loading'
 import Colors from '../constants/Colors'
+import helpers from '../utils/helpers'
 
 export default class WebviewScreen extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      webUrl: 'https://www.indonesiadefenceservices.com/'
+    }
+  }
+  componentDidMount(){
+    if(helpers.isAndroid){
+      BackHandler.addEventListener('hardwareBackPress', () => {
+        if (!this.onMainScreen()) {
+          this.goBack();
+          return true;
+        }
+        return false;
+      });
+    }
+  }
+  componentWillUnmount(){
+    if(helpers.isAndroid){
+      BackHandler.removeEventListener('hardwareBackPress', () => {
+        return false;
+      });
+    }
+  }
+  onMainScreen(){
+    return this.state.webUrl == 'https://www.indonesiadefenceservices.com/'
+  }
+  goBack(){
+    this.webRef.goBack()
+  }
   render(){
     return (
       <View style={{flex: 1}}>
         <WebView
+          ref={webRef => this.webRef = webRef}
           source={{ uri: 'https://www.indonesiadefenceservices.com/' }}
           startInLoadingState={true}
+          onNavigationStateChange={(webState) => {
+            this.setState({webUrl: webState.url})
+          }}
           renderLoading={() => <Loading />}
         />
       </View>
